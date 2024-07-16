4a4a4b686c10813bb5c110d1bdfa90d1ca056f21
streino <streino@disroot.org>
2024-07-16 17:55:45 +0200

    Move helper functions at the end of file

--- run_tests.py
+++ run_tests.py
@@ -8,6 +8,36 @@ XSLT_DIR = CWD / 'xslt'
 TEST_DIR = CWD / 'test'
 
 
+# Pytest entry point
+def pytest_generate_tests(metafunc):
+  argnames = ['xslt_name', 'test_name']
+  if not set(argnames) <= set(metafunc.fixturenames):
+    raise RuntimeError('Cannot find test function')
+  metafunc.parametrize(argnames, list_test_cases())
+
+
+# Parametrized test function
+def test_transform(xslt_name, test_name):
+  xslt_path = resolve(XSLT_DIR / f'{xslt_name}.xsl')
+  input_path = resolve(TEST_DIR / f'{xslt_name}--{test_name}--input.xml')
+  expected_path = resolve(TEST_DIR /f'{xslt_name}--{test_name}--expected.xml')
+
+  transform = make_transform(xslt_path)
+
+  input_tree = etree.parse(input_path)
+  actual_tree = transform(input_tree)
+  expected_tree = etree.parse(expected_path)
+
+  # print('input:\n', to_string(input_tree))
+  # print('actual:\n', to_string(actual_tree))
+  # print('expected:\n', to_string(expected_tree))
+
+  diff = compare(actual_tree, expected_tree)
+  if diff:
+    print(diff)
+    pytest.fail(f'{xslt_path.name}/{input_path.name}', pytrace=False)
+
+
 # Helper functions
 
 def list_test_cases():
@@ -38,33 +68,3 @@ def compare(actual_tree, expected_tree):
   diff = ndiff(actual_lines, expected_lines)
   has_diff = any(d[0] != ' ' for d in diff)
   return ''.join(diff) if has_diff else None
-
-
-# Pytest entry point
-def pytest_generate_tests(metafunc):
-  argnames = ['xslt_name', 'test_name']
-  if not set(argnames) <= set(metafunc.fixturenames):
-    raise RuntimeError('Cannot find test function')
-  metafunc.parametrize(argnames, list_test_cases())
-
-
-# Parametrized test function
-def test_transform(xslt_name, test_name):
-  xslt_path = resolve(XSLT_DIR / f'{xslt_name}.xsl')
-  input_path = resolve(TEST_DIR / f'{xslt_name}--{test_name}--input.xml')
-  expected_path = resolve(TEST_DIR /f'{xslt_name}--{test_name}--expected.xml')
-
-  transform = make_transform(xslt_path)
-
-  input_tree = etree.parse(input_path)
-  actual_tree = transform(input_tree)
-  expected_tree = etree.parse(expected_path)
-
-  # print('input:\n', to_string(input_tree))
-  # print('actual:\n', to_string(actual_tree))
-  # print('expected:\n', to_string(expected_tree))
-
-  diff = compare(actual_tree, expected_tree)
-  if diff:
-    print(diff)
-    pytest.fail(f'{xslt_path.name}/{input_path.name}', pytrace=False)
