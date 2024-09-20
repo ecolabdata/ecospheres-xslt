2935d566b2e52bd4e14204bf5dc04040e394b818
streino <streino@disroot.org>
2024-09-20 17:34:47 +0200

    Ruff pass

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import pytest
-from colorama import Fore, Back, Style, init
-from difflib import ndiff, unified_diff
+from colorama import Fore, init
+from difflib import unified_diff
 from lxml import etree
 from pathlib import Path
 
@@ -14,74 +14,77 @@ FIXTURE_DIR = TEST_DIR / "fixtures"
 
 # Pytest entry point
 def pytest_generate_tests(metafunc):
-  argnames = ["xslt_name", "fixture_name", "test_suffix"]
-  if not set(argnames) <= set(metafunc.fixturenames):
-    raise RuntimeError("Cannot find test function")
-  metafunc.parametrize(argnames, list_test_cases())
+    argnames = ["xslt_name", "fixture_name", "test_suffix"]
+    if not set(argnames) <= set(metafunc.fixturenames):
+        raise RuntimeError("Cannot find test function")
+    metafunc.parametrize(argnames, list_test_cases())
 
 
-# Parametrized test function
+# Pytest parametrized test function
 def test_transform(xslt_name, fixture_name, test_suffix):
-  xslt_path = resolve(XSLT_DIR / f"{xslt_name}.xsl")
-  fixture_path = resolve(FIXTURE_DIR / f"{fixture_name}.xml")
-  expected_path = resolve(TEST_DIR /f"{xslt_name}--{fixture_name}.{test_suffix}")
+    xslt_path = resolve(XSLT_DIR / f"{xslt_name}.xsl")
+    fixture_path = resolve(FIXTURE_DIR / f"{fixture_name}.xml")
+    expected_path = resolve(TEST_DIR / f"{xslt_name}--{fixture_name}.{test_suffix}")
 
-  transform = make_transform(xslt_path)
+    transform = make_transform(xslt_path)
 
-  fixture_tree = etree.parse(fixture_path)
+    fixture_tree = etree.parse(fixture_path)
 
-  if test_suffix == "xml":
-    actual_tree = transform(fixture_tree)
-    expected_tree = etree.parse(expected_path)
-    compare(actual_tree, expected_tree)
-  elif test_suffix == "err":
-    expected_error = expected_path.open().read().strip()
-    with pytest.raises(etree.XSLTApplyError) as e:
-      transform(fixture_tree)
-    assert str(e.value) == expected_error
+    if test_suffix == "xml":
+        actual_tree = transform(fixture_tree)
+        expected_tree = etree.parse(expected_path)
+        compare(actual_tree, expected_tree)
+    elif test_suffix == "err":
+        expected_error = expected_path.open().read().strip()
+        with pytest.raises(etree.XSLTApplyError) as e:
+            transform(fixture_tree)
+        assert str(e.value) == expected_error
 
-
-# Helper functions
 
 def list_test_cases():
-  for xslt_path in XSLT_DIR.glob("*.xsl"):
-    xslt_name = xslt_path.stem
-    for test_path in TEST_DIR.glob(f"{xslt_name}--*.*"):
-      test_suffix = test_path.suffix[1:]
-      if test_suffix not in ("xml", "err"):
-        continue
-      fixture_name = test_path.stem.split("--")[1]
-      yield pytest.param(xslt_name, fixture_name, test_suffix, id=test_path.stem)
+    for xslt_path in XSLT_DIR.glob("*.xsl"):
+        xslt_name = xslt_path.stem
+        for test_path in TEST_DIR.glob(f"{xslt_name}--*.*"):
+            test_suffix = test_path.suffix[1:]
+            if test_suffix not in ("xml", "err"):
+                continue
+            fixture_name = test_path.stem.split("--")[1]
+            yield pytest.param(xslt_name, fixture_name, test_suffix, id=test_path.stem)
+
 
 def resolve(path):
-  if not path.exists():
-    pytest.skip(f"Missing path: {path}")
-  return path
+    if not path.exists():
+        pytest.skip(f"Missing path: {path}")
+    return path
+
 
 def make_transform(xslt_path):
-  xslt_tree = etree.parse(xslt_path)
-  transform = etree.XSLT(xslt_tree)
-  return lambda tree: transform(tree)
+    xslt_tree = etree.parse(xslt_path)
+    transform = etree.XSLT(xslt_tree)
+    return lambda tree: transform(tree)
+
 
 def to_string(tree):
-  etree.indent(tree)
-  return etree.tostring(tree, pretty_print=True, encoding="unicode")
+    etree.indent(tree)
+    return etree.tostring(tree, pretty_print=True, encoding="unicode")
+
 
 def compare(actual_tree, expected_tree):
-  actual_lines = to_string(actual_tree).splitlines()
-  expected_lines = to_string(expected_tree).splitlines()
-  diff = unified_diff(expected_lines, actual_lines, lineterm="")
-  if any(d[0] != " " for d in diff):
-    print("\n".join(color_diff(diff)))
-    pytest.fail("Unexpected file contents:\n" + "\n".join(diff), pytrace=False)
+    actual_lines = to_string(actual_tree).splitlines()
+    expected_lines = to_string(expected_tree).splitlines()
+    diff = unified_diff(expected_lines, actual_lines, lineterm="")
+    if any(d[0] != " " for d in diff):
+        print("\n".join(color_diff(diff)))
+        pytest.fail("Unexpected file contents:\n" + "\n".join(diff), pytrace=False)
+
 
 def color_diff(diff):
-  for line in diff:
-    if line.startswith('+'):
-      yield Fore.GREEN + line + Fore.RESET
-    elif line.startswith('-'):
-      yield Fore.RED + line + Fore.RESET
-    elif line.startswith('^'):
-      yield Fore.BLUE + line + Fore.RESET
-    else:
-      yield line
+    for line in diff:
+        if line.startswith("+"):
+            yield Fore.GREEN + line + Fore.RESET
+        elif line.startswith("-"):
+            yield Fore.RED + line + Fore.RESET
+        elif line.startswith("^"):
+            yield Fore.BLUE + line + Fore.RESET
+        else:
+            yield line
